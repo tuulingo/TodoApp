9347ac0936f4302d1d40350013f08790e4a00bdf
tuulingo <oliver.tuuling@tptlive.ee>
2022-09-07 14:45:11 +0300

    Add stuff related to AddEditTodo, also add navigation

--- app/src/main/java/com/tuulingo/todoapp/MainActivity.kt
+++ app/src/main/java/com/tuulingo/todoapp/MainActivity.kt
@@ -10,34 +10,47 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.tuulingo.todoapp.ui.add_edit_todo.AddEditTodoScreen
 import com.tuulingo.todoapp.ui.theme.ToDoAppTheme
+import com.tuulingo.todoapp.ui.todo_list.TodoListScreen
+import com.tuulingo.todoapp.util.Routes
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.TODO_LIST
                 ) {
-                    Greeting("Android")
+                    composable(Routes.TODO_LIST) {
+                        TodoListScreen(onNavigate = { navController.navigate(it.route) })
+                    }
+                    composable(
+                        route = Routes.ADD_EDIT_TODO + "?todoId={todoId}",
+                        arguments = listOf(
+                            navArgument(name = "todoId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) {
+                        AddEditTodoScreen(onPopBackStack = {
+                            navController.popBackStack()
+                        })
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ToDoAppTheme {
-        Greeting("Android")
     }
 }

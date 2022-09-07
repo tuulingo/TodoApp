cdaa2a6880db13a0784aec44bc1f8a567ce7a256
tuulingo <oliver.tuuling@tptlive.ee>
2022-09-07 11:01:36 +0300

    Add hilt and dependencies in AppModule

---
+++ app/src/main/java/com/tuulingo/todoapp/TodoApp.kt
@@ -0,0 +1,8 @@
+package com.tuulingo.todoapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TodoApp: Application() {
+}
---
+++ app/src/main/java/com/tuulingo/todoapp/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.tuulingo.todoapp.di
+
+import android.app.Application
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.tuulingo.todoapp.data.TodoDatabase
+import com.tuulingo.todoapp.data.TodoRepository
+import com.tuulingo.todoapp.data.TodoRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app: Application): TodoDatabase
+    {
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db: TodoDatabase): TodoRepository {
+        return TodoRepositoryImpl(db.dao)
+    }
+}

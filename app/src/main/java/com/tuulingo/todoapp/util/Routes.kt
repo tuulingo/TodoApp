5dd9d34b7da2a80fd8472fffeb188c5682671bb1
tuulingo <oliver.tuuling@tptlive.ee>
2022-09-26 00:04:35 +0300

    Add route

--- app/src/main/java/com/tuulingo/todoapp/util/Routes.kt
+++ app/src/main/java/com/tuulingo/todoapp/util/Routes.kt
@@ -1,6 +1,7 @@
 package com.tuulingo.todoapp.util
 
 object Routes {
+    const val CATEGORIES_LIST = "categories_list"
     const val TODO_LIST = "todo_list"
     const val ADD_EDIT_TODO = "add_edit_todo"
 

0ca86e6cc359fad3a1efb8b2e0825dbff09ee3f0
tuulingo <oliver.tuuling@tptlive.ee>
2022-09-28 19:32:51 +0300

    Add category events

--- app/src/main/java/com/tuulingo/todoapp/ui/category/category_list/CategoryEvent.kt
+++ app/src/main/java/com/tuulingo/todoapp/ui/category/category_list/CategoryEvent.kt
@@ -1,4 +1,10 @@
 package com.tuulingo.todoapp.ui.category.category_list
 
-class CategoryEvent {
+import com.tuulingo.todoapp.data.categories.Category
+
+sealed class CategoryEvent {
+    data class OnDeleteCategoryClick(val category: Category): CategoryEvent()
+    object OnUndoDeleteClick: CategoryEvent()
+    data class OnCategoryClick(val category: Category): CategoryEvent()
+    object OnAddCategoryClick: CategoryEvent()
 }
